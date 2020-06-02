267244cf980c48e6a1e32427636f5d9ec72240ca
Lukas Berbuer <lukas.berbuer@gmail.com>
2020-06-02 18:50:07 +0200

    Stream timestamp, add async context manager

--- src/waveline/conditionwave.py
+++ src/waveline/conditionwave.py
@@ -9,22 +9,12 @@ conditionWave
 """
 
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 import logging
 import socket
 from typing import List
 
 import numpy as np
-
-
-CHANNELS = (1, 2)
-MAX_SAMPLERATE = 10_000_000
-RANGES = {
-    0: 0.05,  # 50 mV
-    1: 5.0,  # 5 V
-}
-DEFAULT_RANGE = 0
-PORT = 5432
 
 
 logger = logging.getLogger(__name__)
@@ -33,26 +23,42 @@ logger = logging.getLogger(__name__)
 class ConditionWave:
     """API for conditionWave device."""
 
+    CHANNELS = (1, 2)
+    MAX_SAMPLERATE = 10_000_000
+    RANGES = {
+        0: 0.05,  # 50 mV
+        1: 5.0,  # 5 V
+    }
+    DEFAULT_RANGE = 0
+    PORT = 5432
+
     def __init__(self, address: str):
         self._address = address
         self._reader = None
         self._writer = None
-        self._range = RANGES[DEFAULT_RANGE]
+        self._range = self.RANGES[self.DEFAULT_RANGE]
         self._decimation = 1
         self._daq_active = False
 
-    @staticmethod
-    def discover(timeout: float = 0.5) -> List[str]:
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.close()
+
+    @classmethod
+    def discover(cls, timeout: float = 0.5) -> List[str]:
         message = b"find"
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
         # enable broadcasting mode
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         # bind to port
-        server.bind(("", PORT))
+        server.bind(("", cls.PORT))
 
         # send broadcast message
-        server.sendto(message, ("<broadcast>", PORT))
+        server.sendto(message, ("<broadcast>", cls.PORT))
 
         def get_response(timeout=timeout):
             server.settimeout(timeout)
@@ -74,10 +80,17 @@ class ConditionWave:
         return self._decimation
 
     async def connect(self):
-        logger.info(f"Open connection {self._address}:{PORT}...")
+        logger.info(f"Open connection {self._address}:{self.PORT}...")
         self._reader, self._writer = await asyncio.open_connection(
-            self._address, PORT,
+            self._address, self.PORT,
         )
+    
+    async def close(self):
+        try:
+            self._writer.close()
+            logger.info(f"Connection {self._address}:{self.PORT} closed...")
+        except:
+            pass
 
     async def _write(self, message):
         logger.debug("Write message: %s", message)
@@ -91,11 +104,11 @@ class ConditionWave:
         print(data.decode())
 
     async def set_range(self, range_index: int):
-        if range_index not in RANGES.keys():
+        if range_index not in self.RANGES.keys():
             raise ValueError("Invalid range index.")
-        logger.info(f"Set range to {range_index} ({RANGES[range_index]} V)...")
+        logger.info(f"Set range to {range_index} ({self.RANGES[range_index]} V)...")
         await self._write(f"set_adc_range 0 {range_index:d}")
-        self._range = RANGES[range_index]
+        self._range = self.RANGES[range_index]
 
     async def set_decimation(self, factor: int):
         factor = int(factor)
@@ -109,8 +122,8 @@ class ConditionWave:
         self._daq_active = True
 
     async def stream(self, channel: int, blocksize: int):
-        if channel not in CHANNELS:
-            raise ValueError(f"Channel must be in {CHANNELS}")
+        if channel not in self.CHANNELS:
+            raise ValueError(f"Channel must be in {self.CHANNELS}")
         if not self._daq_active:
             raise RuntimeError("Data acquisition not started")
 
@@ -120,18 +133,20 @@ class ConditionWave:
                 f"(blocksize: {blocksize}, range: {self._range} V)"
             )
         )
-        port = int(PORT + channel)
+        port = int(self.PORT + channel)
         blocksize_bits = int(blocksize * 2)  # 16 bit = 2 * 8 byte
         to_volts = float(self._range) / (2 ** 15)
 
-        timestamp_ms = 1000 * datetime.now().timestamp()
-        interval_ms = 1000 * self.decimation * blocksize / MAX_SAMPLERATE
+        timestamp = None
+        interval = timedelta(seconds=self.decimation * blocksize / self.MAX_SAMPLERATE)
 
         reader, writer = await asyncio.open_connection(self._address, port)
         while True:
             buffer = await reader.readexactly(blocksize_bits)
-            yield timestamp_ms, np.frombuffer(buffer, dtype=np.int16).astype(np.float32) * to_volts
-            timestamp_ms += interval_ms
+            if timestamp is None:
+                timestamp = datetime.now()
+            yield timestamp, np.frombuffer(buffer, dtype=np.int16).astype(np.float32) * to_volts
+            timestamp += interval
         writer.close()
 
     async def stop_acquisition(self):
@@ -140,8 +155,4 @@ class ConditionWave:
         self._daq_active = False
 
     def __del__(self):
-        logger.info("Close connection...")
-        try:
-            self._writer.close()
-        except:
-            pass
+        self.close()

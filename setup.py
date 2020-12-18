0bcaf4065c3deea54e04922849b49b4dd7540f9e
Lukas Berbuer <lukas.berbuer@gmail.com>
2020-12-18 13:04:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+with open(HERE / "README.md", encoding="utf-8") as f:
+    LONG_DESCRIPTION = f.read()
 
 INSTALL_REQUIRES = [
     "numpy",
@@ -31,12 +37,40 @@ EXTRAS_REQUIRE["dev"] = EXTRAS_REQUIRE["docs"] + EXTRAS_REQUIRE["tests"] + EXTRA
 setup(
     name="waveline",
     version="0.1.3",
-    description="API for Vallen Systeme waveLine devices (conditionWave, spotWave)",
+    description="Library to easily interface with Vallen Systeme WaveLine™ devices",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vallen-systems/pyWaveline",
     author="Lukas Berbuer (Vallen Systeme GmbH)",
     author_email="software@vallen.de",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "Vallen",
+        "Acoustic Emission",
+        "ultrasonic",
+        "data acquisition",
+        "WaveLine",
+        "conditionWave",
+        "spotWave",
+    ],
     packages=find_packages("src"),
     package_dir={"": "src"},
     python_requires=">=3.6",
     install_requires=INSTALL_REQUIRES,
     extras_require=EXTRAS_REQUIRE,
+    project_urls={
+        "Bug Reports": "https://github.com/vallen-systems/pyWaveline/issues",
+        "Source": "https://github.com/vallen-systems/pyWaveline",
+    },
 )

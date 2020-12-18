8c37c1edb144f4b8d994c3353a8d7a191971cf5c
Lukas Berbuer <lukas.berbuer@gmail.com>
2020-12-18 10:57:01 +0100

    Update utils functions, add tests

--- src/waveline/utils.py
+++ src/waveline/utils.py
@@ -1,9 +1,29 @@
+from typing import Union
+
 import numpy as np
 
 
-def decibel_to_volts(decibel):
-    return 1e-6 * 10 ** (decibel / 20)
+def decibel_to_volts(decibel: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+    """
+    Convert from dB(AE) to volts.
+
+    Args:
+        decibel: Input in decibel, scalar or array
+
+    Returns:
+        Input value(s) in volts
+    """
+    return 1e-6 * np.power(10, np.asarray(decibel) / 20)
 
 
-def volts_to_decibel(volts):
-    return 20 ** np.log10(volts * 1e6)
+def volts_to_decibel(volts: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
+    """
+    Convert from volts to dB(AE).
+
+    Args:
+        volts: Inpult in volts, scalar or array
+
+    Returns:
+        Input value(s) in dB(AE)
+    """
+    return 20 * np.log10(np.asarray(volts) * 1e6)

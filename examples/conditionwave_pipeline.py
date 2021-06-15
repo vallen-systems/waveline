d5bddda16c1f41261dc2d76896b768c469c7218d
Lukas Berbuer <lukas.berbuer@gmail.com>
2021-06-15 17:47:39 +0200

    Add conditionWave pipeline skeleton/example

---
+++ examples/conditionwave_pipeline.py
@@ -0,0 +1,139 @@
+"""
+Skeleton pipeline with data acquisition, feature extraction, classification and OPC UA output.
+
+The pipeline can run on an external PC or directly on the conditionWave (using the IP 127.0.0.1).
+Please contact Vallen System <software@vallen.de> for more details.
+
+Make sure to install `asyncua` for the OPC UA server:
+
+    pip install asyncua>=0.9
+"""
+
+import argparse
+import asyncio
+import logging
+from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass
+
+import numpy as np
+from asyncua import Server
+from waveline import ConditionWave
+
+SAMPLERATE = 1_000_000  # 1 MHz
+BLOCKSIZE = 524288  # = 2^19 (use power of 2 for efficient FFT computation)
+RANGE = 0.05  # 50 mV
+
+logging.basicConfig(level=logging.INFO)
+
+
+@dataclass
+class Features:
+    """Feature vector."""
+
+    rms: float
+    crest: float
+
+
+@dataclass
+class Result:
+    """Classification result."""
+
+    ok: bool
+
+
+@dataclass
+class Output:
+    """Container for OPC UA outputs."""
+
+    features: Features
+    result: Result
+
+
+def extract_features(signal: np.ndarray) -> Features:
+    """Feature extraction."""
+    peak = np.max(np.abs(signal))
+    rms = np.sqrt(np.mean(signal ** 2))
+    return Features(
+        rms=rms,
+        crest=peak / rms,
+    )
+
+
+def classify(features: Features) -> Result:
+    """Inference based on feature vector, just a placeholder for your logic/model."""
+    if features.rms >= 1e-3:  # >= 1 mV
+        return Result(ok=False)
+    return Result(ok=True)
+
+
+async def pipeline(ip: str, queue: asyncio.Queue):
+    """Pipeline: data acquisition, feature extraction and classification."""
+    async with ConditionWave(ip) as cw:
+        print(await cw.get_info())
+        await cw.set_range(0, range_volts=RANGE)
+        await cw.set_decimation(0, factor=int(cw.MAX_SAMPLERATE / SAMPLERATE))
+        await cw.set_filter(0, highpass=100e3, lowpass=500e3, order=8)
+        await cw.start_acquisition()
+
+        with ThreadPoolExecutor(max_workers=1) as pool:
+            loop = asyncio.get_event_loop()
+            # read streaming data in blocks
+            async for timestamp, y in cw.stream(1, BLOCKSIZE):
+                # execute (longer) blocking operations in the thread pool -> don't block event loop
+                features = await loop.run_in_executor(pool, extract_features, y)
+                result = await loop.run_in_executor(pool, classify, features)
+                print(timestamp, features, result)
+                # push output to queue
+                queue.put_nowait(Output(features=features, result=result))
+
+        await cw.stop_acquisition()
+
+
+async def opcua_server(queue: asyncio.Queue):
+    """OPC UA server."""
+    # setup server
+    server = Server()
+    await server.init()
+    server.set_endpoint("opc.tcp://0.0.0.0:4840")
+    server.set_server_name("conditionWave OPC UA server")
+
+    # setup nodes
+    uri = "https://www.vallen.de"
+    idx = await server.register_namespace(uri)
+    root_node = server.nodes.objects
+    features_node = await root_node.add_folder(idx, "features")
+    rms_node = await features_node.add_variable(idx, "features:rms", 0.0)
+    crest_node = await features_node.add_variable(idx, "features:crest", 0.0)
+    result_node = await root_node.add_folder(idx, "result")
+    ok_node = await result_node.add_variable(idx, "result:ok", True)
+
+    # wait for data and write to nodes
+    async with server:
+        while True:
+            # get output from queue
+            output: Output = await queue.get()
+            queue.task_done()
+            await rms_node.write_value(float(output.features.rms))
+            await crest_node.write_value(float(output.features.crest))
+            await ok_node.write_value(bool(output.result.ok))
+
+
+async def main(ip: str):
+    """Launch pipeline and OPC UA server."""
+    output_queue: asyncio.Queue = asyncio.Queue()  # decouple acquisition/processing and io
+
+    await asyncio.gather(
+        pipeline(ip, output_queue),
+        opcua_server(output_queue),
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="conditionwave_pipeline")
+    parser.add_argument("ip", help="IP address of conditionWave device")
+    args = parser.parse_args()
+
+    try:
+        asyncio.run(main(args.ip))
+    except KeyboardInterrupt:
+        ...

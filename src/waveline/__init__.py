b4a2684f2cdb8888f1990ca4a8f8393fe9917f02
Lukas Berbuer <lukas.berbuer@gmail.com>
2024-03-20 14:58:01 +0100

    feat!: remove deprecated conditionwave module

--- src/waveline/__init__.py
+++ src/waveline/__init__.py
@@ -3,7 +3,6 @@ Top-level module of waveline.
 """
 
 # ruff: noqa: F401
-from .conditionwave import ConditionWave
 from .datatypes import AERecord, TRRecord
 from .linwave import LinWave
 from .spotwave import SpotWave

0a726fd4f8379c61828f55ab9f17f0a544b4fa42
Lukas Berbuer <lukas.berbuer@gmail.com>
2025-05-26 09:33:36 +0200

    perf: efficient hit merging in linwave_ae example

--- examples/linwave_ae.py
+++ examples/linwave_ae.py
@@ -19,13 +19,17 @@ from __future__ import annotations
 import argparse
 import asyncio
 import logging
+from collections import defaultdict, deque
 from dataclasses import asdict, dataclass
+from typing import AsyncGenerator
 
 import numpy as np
 
 from waveline import AERecord, LinWave, TRRecord
 
+logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
+np.set_printoptions(threshold=10)  # print max 10 elements
 
 
 @dataclass
@@ -36,31 +40,43 @@ class HitRecord(AERecord):
     data: np.ndarray
 
 
-async def merge_ae_tr_records(async_generator):
-    """Helper function to merge matching AERecords and TRRecords (same trai)."""
-    dict_ae: dict[int, AERecord] = {}
-    dict_tr: dict[int, TRRecord] = {}
+async def merge_ae_tr_records(
+    generator: AsyncGenerator[AERecord | TRRecord, None],
+) -> AsyncGenerator[HitRecord, None]:
+    """
+    Helper function to merge matching AERecords and TRRecords (same trai).
 
-    async for record in async_generator:
+    AE and TR records will be received in the same order per channel. So the AE records will be
+    stored channel-wise in a deque and merged with the TR records when available.
+    """
+    ae_queues: defaultdict[int, deque[AERecord]] = defaultdict(deque)
+
+    async for record in generator:
         if isinstance(record, AERecord):
             if record.trai == 0:  # status data or hit without transient data -> return directly
-                yield record
+                yield HitRecord(**asdict(record), samples=0, data=np.array([]))
             else:
-                dict_ae[record.trai] = record  # store in buffer to merge later
+                ae_queues[record.channel].append(record)
         if isinstance(record, TRRecord):
-            dict_tr[record.trai] = record  # store in buffer to merge later
+            ae_queue = ae_queues[record.channel]
+            logger.info("AE queue size for channel %d: %s", record.channel, len(ae_queue))
+            while ae_queue and ae_queue[0].trai < record.trai:
+                ae_queue.popleft()
+                logger.warning("Missing TR record for trai %d, discard AE", record.trai)
+            if not ae_queue:
+                logger.warning("AE record queue empty")
+                continue
+            if ae_queue[0].trai > record.trai:
+                logger.warning("Missing AE record for trai %d, discard TR", record.trai)
+                continue
 
-        # try to match and return merged HitRecords
-        trais_ae = set(dict_ae.keys())
-        trais_tr = set(dict_tr.keys())
-        trais_match = trais_ae.intersection(trais_tr)
-        for trai in trais_match:
-            ae_record = dict_ae.pop(trai)
-            tr_record = dict_tr.pop(trai)
+            ae_record = ae_queue.popleft()
+            assert ae_record.trai == record.trai
+            assert ae_record.channel == record.channel
             yield HitRecord(
                 **asdict(ae_record),
-                samples=tr_record.samples,
-                data=tr_record.data,
+                samples=record.samples,
+                data=record.data,
             )
 
 
